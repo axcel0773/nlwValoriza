280f663b6e5de0ccc18662e5a9a713e79135beb9
axcel0773 <yuji-watanabe1@hotmail.com>
2021-09-10 22:35:17 -0300

    project configuration and creation of routes

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+// @types/express
+const app = express();
+
+
+/* 
+    GET = Buscar uma informação
+    POST = Inserir (Criar) uma informação
+    PUT = Alterar uma informação
+    DELETE = Remover um dado
+    PATCH = Alterar uma informação específica
+*/
+
+app.get("/test", (request, response) => {
+    //Request => Entrando
+    //Response => Saindo
+    return response.send("Olá mundo");
+});
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá mundo método POST");
+});
+
+//http://localhost:3000
+app.listen(3000, () => console.log("O pai ta on"));
